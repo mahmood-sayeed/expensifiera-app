3577e7bcd7ac06c2158511f8544cfa54099cb595
mahmood-sayeed <mahmood.sayeed8@gmail.com>
2020-12-06 21:28:18 +0530

    Link & NavLink

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -32,12 +32,24 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-        404!
+        404 - <Link to="/">Go Home</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensifiera</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
 );
 
 const routes = (
     <BrowserRouter>
+    <div>
+        <Header/>
         <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
@@ -45,6 +57,7 @@ const routes = (
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
         </Switch>
+    </div>
     </BrowserRouter>
 );
 
