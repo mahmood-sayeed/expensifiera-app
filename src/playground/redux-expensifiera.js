3251e095912208bbe93f6b93daba1998dcaffa37
mahmood-sayeed <mahmood.sayeed8@gmail.com>
2020-12-11 01:10:53 +0530

    ES6 Spread operator in Array

--- src/playground/redux-expensifiera.js
+++ src/playground/redux-expensifiera.js
@@ -1,7 +1,31 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 //ADD_EXPENSE
+const addExpense = (
+    { 
+        description = '', 
+        note = '', 
+        amount = 0, 
+        createdAt = 0
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
 //REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
 //EDIT_EXPENSE
 //SET_TEXT_FILTER
 //SORT_BY_DATE
@@ -15,6 +39,14 @@ const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,               //"..." spread operator. does not change original array. works like concat
+                action.expense
+            ];
+        
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id);
         default:
             return state;
     }
@@ -45,7 +77,15 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
 
 const demoState = {
     expenses: [{
