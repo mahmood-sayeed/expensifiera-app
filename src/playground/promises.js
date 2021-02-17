2d85cfb71eed282a7d3e4019ab69f331a06031c7
mahmood-sayeed <mahmood.sayeed8@gmail.com>
2021-02-17 16:51:42 +0530

    testing async redux actions completed.

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -13,11 +13,25 @@ console.log('before');
 //resolve callback
 promise.then((data) => {
     console.log('1', data);
-    return 'some data';     
+    return 'some data';     //this text gets passed down to str
 }).then((str) => {
     console.log('2', str);
 }).catch((error) => {
     console.log('error: ', error);  //if reject then catch
+});
+
+//you can also return an entirely new promise in which the success case would be the next 'then'
+promise.then((data) => {
+    console.log('1', data);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('some other promise');
+        }, 5500);
+    });     
+}).then((str) => {
+    console.log('2', str);
+}).catch((error) => {
+    console.log('error: ', error);  
 });
 
 //second way to catch
