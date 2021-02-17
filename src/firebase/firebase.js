5b819f9ecec00b14b377238a9bfb29f2dccca623
mahmood-sayeed <mahmood.sayeed8@gmail.com>
2021-02-17 21:27:34 +0530

    Creating, config separate test db & Heroku env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBW12ibkAziCBetN-jJSOvypmJlGHTOiPw",
-    authDomain: "expensifiera.firebaseapp.com",
-    databaseURL: "https://expensifiera-default-rtdb.firebaseio.com",
-    projectId: "expensifiera",
-    storageBucket: "expensifiera.appspot.com",
-    messagingSenderId: "199976993885",
-    appId: "1:199976993885:web:d17e1c36a4c34d42b6b9b4",
-    measurementId: "G-NZ0HNFK9KF"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
